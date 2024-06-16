41f1c646c10e91114a0f4e97fbcabb0f9ef8bb07
TNAlthea <sabiannaya@yahoo.com>
2024-06-16 22:58:49 +0700

    feat(routes): new routes for index, home, berita, detail_berita, detail_lomba

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,11 +5,15 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
-$routes->get('/test', 'Home::test');
-$routes->get('/berita', 'Home::berita');
+$routes->get('/', 'IndexController::index');
 
 $routes->group('admin', function ($routes) {
     $routes->get('dashboard', 'AdminController::dashboard');
-    // $routes->get('product/(:num)', 'AdminController::product/$1');
+});
+
+$routes->group('home', function ($routes) {
+    $routes->get('', 'HomeController::index');
+    $routes->get('berita', 'HomeController::berita');
+    $routes->get('detail_berita', 'HomeController::detail_berita');
+    $routes->get('detail_lomba', 'HomeController::detail_lomba');
 });
