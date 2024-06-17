459292c1a331da458971d24a337f3b1f02a143d5
TNAlthea <sabiannaya@yahoo.com>
2024-06-17 19:14:43 +0700

    feat(view): added form_tim view page

---
+++ app/Views/Form/form_tim.php
@@ -0,0 +1,292 @@
+<!doctype html>
+<html lang="zxx">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>aranoz</title>
+  <link rel="icon" href="<?= base_url('img/favicon.png') ?>">
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="<?= base_url('css/bootstrap.min.css') ?>">
+  <!-- animate CSS -->
+  <link rel="stylesheet" href="<?= base_url('css/animate.css') ?>">
+  <!-- owl carousel CSS -->
+  <link rel="stylesheet" href="<?= base_url('css/owl.carousel.min.css') ?>">
+  <!-- font awesome CSS -->
+  <link rel="stylesheet" href="<?= base_url('css/all.css') ?>">
+  <!-- flaticon CSS -->
+  <link rel="stylesheet" href="<?= base_url('css/flaticon.css') ?>">
+  <link rel="stylesheet" href="<?= base_url('css/themify-icons.css') ?>">
+  <!-- font awesome CSS -->
+  <link rel="stylesheet" href="<?= base_url('css/magnific-popup.css') ?>">
+  <!-- swiper CSS -->
+  <link rel="stylesheet" href="<?= base_url('css/slick.css') ?>">
+  <!-- style CSS -->
+  <link rel="stylesheet" href="<?= base_url('css/style.css') ?>">
+</head>
+
+<body>
+  <!--::header part start::-->
+  <header class="main_menu home_menu">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-lg-12">
+          <nav class="navbar navbar-expand-lg navbar-light">
+            <a class="navbar-brand" href="<?= base_url('home') ?>">
+              <img src="<?= base_url('img/logo.png') ?>" alt="logo" />
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="menu_icon"><i class="fas fa-bars"></i></span>
+            </button>
+
+            <div class="collapse navbar-collapse main-menu-item" id="navbarSupportedContent">
+              <ul class="navbar-nav">
+                <li class="nav-item">
+                  <a class="nav-link" href="<?= base_url('home') ?>">Halaman Utama</a>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown_1" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Kategori
+                  </a>
+                  <div class="dropdown-menu" aria-labelledby="navbarDropdown_1">
+                    <a class="dropdown-item" href="<?= base_url('home/kategori_akademik') ?>">Akademik</a>
+                    <a class="dropdown-item" href="<?= base_url('home/kategori_nonakademik') ?>">Non-Akademik</a>
+                  </div>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="<?= base_url('home/berita') ?>">Berita</a>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown_1" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Form Pengajuan
+                  </a>
+                  <div class="dropdown-menu" aria-labelledby="navbarDropdown_1">
+                    <a class="dropdown-item" href="<?= base_url('form/form_lomba') ?>">Info Lomba</a>
+                    <a class="dropdown-item" href="<?= base_url('form/form_tim') ?>">Tim Lomba</a>
+                  </div>
+                </li>
+              </ul>
+            </div>
+            <div class="hearer_icon d-flex">
+              <a id="search_1" href="javascript:void(0)"><i class="ti-search"></i></a>
+              <a href=""><i class="ti-bell"></i></a>
+              <div class="dropdown cart">
+                <a class="dropdown-toggle" href="#" id="navbarDropdown3" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  <a href="<?= base_url('mahasiswa/profile') ?>" class="icon-link">
+                    <i class="ti-user"></i>
+                  </a>
+                </a>
+              </div>
+            </div>
+          </nav>
+        </div>
+      </div>
+    </div>
+
+
+  </header>
+  <!-- Header part end-->
+
+  <section class="form">
+    <div class="container">
+      <div class="judul_form_lomba">
+        <!-- Bagian judul form lainnya di sini -->
+      </div>
+
+      <form action="inserttim.php" method="POST" class="form_lomba">
+        <section class="pb-3">
+          <div class="input-box">
+            <label>Nama Lomba</label>
+            <input type="text" name="nama_lomba" placeholder="Cantumkan Nama Lomba">
+          </div>
+
+          <div class="input-box">
+            <label>Nama Tim</label>
+            <input type="text" name="nama_tim" placeholder="Cantumkan Nama Tim">
+          </div>
+        </section>
+
+        <section id="ketua" class="pb-3">
+          <div class="input-box">
+            <label>Nama Ketua</label>
+            <input type="text" name="nama_ketua" placeholder="Cantumkan Nama Ketua">
+          </div>
+
+          <div class="d-flex flex-column input-box">
+            <label>Prodi Ketua</label>
+            <select class="custom-select" name="prodi_anggota_1" name="prodi_ketua">
+              <option value="Pilih prodi" disabled selected>Pilih Prodi</option>
+              <?php foreach ($result as $row) : ?>
+                <option value="<?= $row['prodi_id'] ?>"><?= $row['nama_prodi'] ?></option>
+              <?php endforeach; ?>
+            </select>
+          </div>
+        </section>
+
+        <section id="anggota_1" class="pb-3">
+          <div class="input-box">
+            <label>Nama Anggota 1</label>
+            <input type="text" name="anggota_1" placeholder="Cantumkan Nama Anggota">
+          </div>
+
+          <div class="d-flex flex-column input-box">
+            <label>Prodi Anggota</label>
+            <select class="custom-select" name="prodi_anggota_1">
+              <option value="Pilih prodi" disabled selected>Pilih Prodi</option>
+              <?php foreach ($result as $row) : ?>
+                <option value="<?= $row['prodi_id'] ?>"><?= $row['nama_prodi'] ?></option>
+              <?php endforeach; ?>
+            </select>
+          </div>
+        </section>
+
+        <section id="anggota_2" class="pb-3">
+          <div class="input-box">
+            <label>Nama Anggota 2</label>
+            <input type="text" name="anggota_2" placeholder="Cantumkan Nama Anggota">
+          </div>
+
+          <div class="d-flex flex-column input-box">
+            <label>Prodi Anggota</label>
+            <select class="custom-select" name="prodi_anggota_2">
+              <option value="Pilih prodi" disabled selected>Pilih Prodi</option>
+              <?php foreach ($result as $row) : ?>
+                <option value="<?= $row['prodi_id'] ?>"><?= $row['nama_prodi'] ?></option>
+              <?php endforeach; ?>
+            </select>
+          </div>
+        </section>
+
+        <section id="anggota_3" class="pb-3">
+          <div class="input-box">
+            <label>Nama Anggota 3</label>
+            <input type="text" name="anggota_3" placeholder="Cantumkan Nama Anggota">
+          </div>
+
+          <div class="d-flex flex-column input-box">
+            <label>Prodi Anggota</label>
+            <select class="custom-select" name="prodi_anggota_3">
+              <option value="Pilih prodi" disabled selected>Pilih Prodi</option>
+              <?php foreach ($result as $row) : ?>
+                <option value="<?= $row['prodi_id'] ?>"><?= $row['nama_prodi'] ?></option>
+              <?php endforeach; ?>
+            </select>
+          </div>
+        </section>
+
+        <button type="submit" class="btn btn-primary">Kirim</button>
+      </form>
+    </div>
+  </section>
+  <!-- Form part end -->
+
+  <!--::footer_part start::-->
+  <footer class="footer_part">
+    <div class="container">
+      <div class="row justify-content-around">
+        <div class="col-sm-6 col-lg-2">
+          <div class="single_footer_part">
+            <ul class="list-unstyled">
+              <li>
+                <h4>Kami Membantu Anda dalam meraih prestasi.</h4>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-sm-6 col-lg-2">
+          <div class="single_footer_part">
+            <h4>Informasi</h4>
+            <ul class="list-unstyled">
+              <li><a hhref="<?= base_url('home/kategori_akademik') ?>">Akademik</a></li>
+              <li><a href="<?= base_url('home/kategori_nonakademik') ?>">Non-Akademik</a></li>
+              <li><a href="<?= base_url('home/berita') ?>">Berita</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-sm-6 col-lg-4">
+          <div class="single_footer_part">
+            <h4>Feedback</h4>
+            <p>
+              Heaven fruitful doesn't over lesser in days. Appear creeping
+            </p>
+            <div id="mc_embed_signup">
+              <form target="_blank" action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01" method="get" class="subscribe_form relative mail_part">
+                <input type="email" name="email" id="newsletter-form-email" placeholder="Masukkan Feedback" class="placeholder hide-on-focus" onfocus="this.placeholder = ''" onblur="this.placeholder = ' Email Address '" />
+                <button type="submit" name="submit" id="newsletter-submit" class="email_icon newsletter-submit button-contactForm">
+                  Kirim
+                </button>
+                <div class="mt-10 info"></div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="copyright_part">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8">
+            <div class="copyright_text">
+              <p>
+                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                Copyright &copy;
+                <script>
+                  document.write(new Date().getFullYear());
+                </script>
+                All Rights Reserved Term of use Union.
+                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+              </p>
+            </div>
+          </div>
+          <div class="col-lg-4">
+            <div class="footer_icon social_icon">
+              <ul class="list-unstyled">
+                <li>
+                  <a href="#" class="single_social_icon"><i class="fab fa-facebook-f"></i></a>
+                </li>
+                <li>
+                  <a href="#" class="single_social_icon"><i class="fab fa-twitter"></i></a>
+                </li>
+                <li>
+                  <a href="#" class="single_social_icon"><i class="fas fa-globe"></i></a>
+                </li>
+                <li>
+                  <a href="#" class="single_social_icon"><i class="fab fa-behance"></i></a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <!--::footer_part end::-->
+  <!-- jquery plugins here-->
+  <!-- jquery -->
+  <script src="<?= base_url('js/jquery-1.12.1.min.js') ?>"></script>
+  <!-- popper js -->
+  <script src="<?= base_url('js/popper.min.js') ?>"></script>
+  <!-- bootstrap js -->
+  <script src="<?= base_url('js/bootstrap.min.js') ?>"></script>
+  <!-- easing js -->
+  <script src="<?= base_url('js/jquery.magnific-popup.js') ?>"></script>
+  <!-- swiper js -->
+  <script src="<?= base_url('js/swiper.min.js') ?>"></script>
+  <!-- swiper js -->
+  <script src="<?= base_url('js/masonry.pkgd.js') ?>"></script>
+  <!-- particles js -->
+  <script src="<?= base_url('js/owl.carousel.min.js') ?>"></script>
+  <!-- slick js -->
+  <script src="<?= base_url('js/jquery.counterup.min.js') ?>"></script>
+  <script src="<?= base_url('js/waypoints.min.js') ?>"></script>
+  <script src="<?= base_url('js/contact.js') ?>"></script>
+  <script src="<?= base_url('js/jquery.ajaxchimp.min.js') ?>"></script>
+  <script src="<?= base_url('js/jquery.form.js') ?>"></script>
+  <script src="<?= base_url('js/jquery.validate.min.js') ?>"></script>
+  <script src="<?= base_url('js/mail-script.js') ?>"></script>
+  <!-- custom js -->
+
+</body>
+
+</html>
