1fdf861ebd8c52c0c856a08b2e455752b56b29b2
TNAlthea <sabiannaya@yahoo.com>
2024-06-17 19:13:29 +0700

    feat(view): added profile_info_lomba view page

---
+++ app/Views/Mahasiswa/profile_info_lomba.php
@@ -0,0 +1,287 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <title>Union</title>
+  <link rel="icon" href="<?= base_url('img/favicon.png') ?>" />
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="<?= base_url('css/bootstrap.min.css') ?>" />
+  <!-- animate CSS -->
+  <link rel="stylesheet" href="<?= base_url('css/animate.css') ?>" />
+  <!-- owl carousel CSS -->
+  <link rel="stylesheet" href="<?= base_url('css/owl.carousel.min.css') ?>" />
+  <!-- font awesome CSS -->
+  <link rel="stylesheet" href="<?= base_url('css/all.css') ?>" />
+  <!-- flaticon CSS -->
+  <link rel="stylesheet" href="<?= base_url('css/flaticon.css') ?>" />
+  <link rel="stylesheet" href="<?= base_url('css/themify-icons.css') ?>" />
+  <!-- font awesome CSS -->
+  <link rel="stylesheet" href="<?= base_url('css/magnific-popup.css') ?>" />
+  <!-- swiper CSS -->
+  <link rel="stylesheet" href="<?= base_url('css/slick.css') ?>" />
+  <!-- style CSS -->
+  <link rel="stylesheet" href="<?= base_url('css/style.css') ?>" />
+  <link rel="stylesheet" href="<?= base_url('css/responsive.css') ?>" />
+  <link rel="stylesheet" href="<?= base_url('css/profile.css') ?>" />
+
+</head>
+
+<body>
+  <header class="main_menu home_menu">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-lg-12">
+          <nav class="navbar navbar-expand-lg navbar-light">
+            <a class="navbar-brand" href="<?= base_url('home') ?>">
+              <img src="<?= base_url('img/logo.png') ?>" alt="logo" />
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="menu_icon"><i class="fas fa-bars"></i></span>
+            </button>
+
+            <div class="collapse navbar-collapse main-menu-item" id="navbarSupportedContent">
+              <ul class="navbar-nav">
+                <li class="nav-item">
+                  <a class="nav-link" href="<?= base_url('home') ?>">Halaman Utama</a>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown_1" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Kategori
+                  </a>
+                  <div class="dropdown-menu" aria-labelledby="navbarDropdown_1">
+                    <a class="dropdown-item" href="<?= base_url('home/kategori_akademik') ?>">Akademik</a>
+                    <a class="dropdown-item" href="<?= base_url('home/kategori_nonakademik') ?>">Non-Akademik</a>
+                  </div>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="<?= base_url('home/berita') ?>">Berita</a>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown_1" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Form Pengajuan
+                  </a>
+                  <div class="dropdown-menu" aria-labelledby="navbarDropdown_1">
+                    <a class="dropdown-item" href="<?= base_url('form/form_lomba') ?>">Info Lomba</a>
+                    <a class="dropdown-item" href="<?= base_url('form/form_tim') ?>">Tim Lomba</a>
+                  </div>
+                </li>
+              </ul>
+            </div>
+            <div class="hearer_icon d-flex">
+              <a id="search_1" href="javascript:void(0)"><i class="ti-search"></i></a>
+              <a href=""><i class="ti-bell"></i></a>
+              <div class="dropdown cart">
+                <a class="dropdown-toggle" href="#" id="navbarDropdown3" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  <a href="<?= base_url('mahasiswa/profile') ?>" class="icon-link">
+                    <i class="ti-user"></i>
+                  </a>
+                </a>
+              </div>
+            </div>
+          </nav>
+        </div>
+      </div>
+    </div>
+
+    <div class="search_input" id="search_input_box">
+      <div class="container">
+        <form class="d-flex justify-content-between search-inner">
+          <input type="text" class="form-control" id="search_input" placeholder="Search Here" />
+          <button type="submit" class="btn"></button>
+          <span class="ti-close" id="close_search" title="Close Search"></span>
+        </form>
+      </div>
+    </div>
+  </header>
+
+  <div class="container-fluid">
+    <div class="row flex-wrap">
+      <div class="bg-dark col-auto col-md-2 col-lg-2 min-vh-100 d-flex flex-column justify-content-between">
+        <ul class="nav nav-pills flex-column mt-4">
+          <li class="nav-item py-2 py-sm-0 my-3 my-sm-4 my-lg-4">
+            <a href="<?= base_url('mahasiswa/profile') ?>" class="nav-link text-center text-white">
+              <img src="<?= base_url('sidebar/assets/Customer.svg') ?>" alt="" class="icon img-fluid" width="70" height="70"><br>
+              <span class="fs-4 d-none d-sm-inline">PROFIL</span>
+            </a>
+          </li>
+          <li class="nav-item py-2 py-sm-0 my-4 my-sm-4 my-lg-3">
+            <a href="<?= base_url('mahasiswa/profile_info_lomba') ?>" class="nav-link text-center text-white">
+              <img src="<?= base_url('sidebar/assets/Edit Property.svg') ?>" alt="" class="icon img-fluid" width="70" height="70"><br>
+              <span class="fs-4 d-none d-sm-inline">PENGAJUAN<br>LOMBA</span>
+            </a>
+          </li>
+          <li class="nav-item py-2 py-sm-0 my-4 my-sm-4 my-lg-3">
+            <a href="<?= base_url('mahasiswa/profile_tim_lomba') ?>" class="nav-link text-center text-white">
+              <img src="<?= base_url('sidebar/assets/People.svg') ?>" alt="" class="icon img-fluid" width="70" height="70"><br>
+              <span class="fs-4 d-none d-sm-inline">TIM</span>
+            </a>
+          </li>
+          <li class="nav-item py-2 py-sm-0 my-4 my-sm-4 my-lg-3">
+            <a href="#" class="nav-link text-center text-white">
+              <img src="<?= base_url('sidebar/assets/Settings.svg') ?>" alt="" class="icon img-fluid" width="70" height="70"><br>
+              <span class="fs-4 d-none d-sm-inline">PENGATURAN</span>
+            </a>
+          </li>
+          <li class="nav-item py-2 py-sm-0 my-4 my-sm-4 my-lg-3">
+            <a href="<?= base_url('auth/mahasiswa/logout') ?>" class="nav-link text-center text-white">
+              <img src="<?= base_url('sidebar/assets/Logout.svg') ?>" alt="" class="icon img-fluid" width="70" height="70"><br>
+              <span class="fs-4 d-none d-sm-inline">KELUAR</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="main-content col-auto min-vh-100 d-flex flex-column p-4 p-sm-4 p-md-5 p">
+        <div class="container">
+          <h1 class="py-5">Informasi Lomba</h1>
+          <table class="info-lomba">
+            <thead>
+              <tr>
+                <th>Nama Lomba</th>
+                <th>Tanggal Lomba</th>
+                <th>Tenggat Pendaftaran</th>
+                <th>Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Lomba Matematika</td>
+                <td>20 Juni 2024</td>
+                <td>17 Juni 2024</td>
+                <td class="status disetujui">Disetujui</td>
+              </tr>
+              <tr>
+                <td>Lomba Sains</td>
+                <td>25 Juni 2024</td>
+                <td>20 Juni 2024</td>
+                <td class="status pending">Pending</td>
+              </tr>
+              <tr>
+                <td>Lomba Menulis</td>
+                <td>30 Juni 2024</td>
+                <td>25 Juni 2024</td>
+                <td class="status tidak-disetujui">Tidak Disetujui</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+
+  <!--::footer_part start::-->
+  <footer class="footer_part">
+    <div class="container">
+      <div class="row justify-content-around">
+        <div class="col-sm-6 col-lg-2">
+          <div class="single_footer_part">
+            <ul class="list-unstyled">
+              <li>
+                <h4>Kami Membantu Anda dalam meraih prestasi.</h4>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-sm-6 col-lg-2">
+          <div class="single_footer_part">
+            <h4>Informasi</h4>
+            <ul class="list-unstyled">
+              <li><a href="">Akademik</a></li>
+              <li><a href="">Non-Akademik</a></li>
+              <li><a href="">Berita</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-sm-6 col-lg-4">
+          <div class="single_footer_part">
+            <h4>Feedback</h4>
+            <p>
+              Heaven fruitful doesn't over lesser in days. Appear creeping
+            </p>
+            <div id="mc_embed_signup">
+              <form target="_blank" action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01" method="get" class="subscribe_form relative mail_part">
+                <input type="email" name="email" id="newsletter-form-email" placeholder="Masukkan Feedback" class="placeholder hide-on-focus" onfocus="this.placeholder = ''" onblur="this.placeholder = ' Email Address '" />
+                <button type="submit" name="submit" id="newsletter-submit" class="email_icon newsletter-submit button-contactForm">
+                  Kirim
+                </button>
+                <div class="mt-10 info"></div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="copyright_part">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8">
+            <div class="copyright_text">
+              <p>
+                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                Copyright &copy;
+                <script>
+                  document.write(new Date().getFullYear());
+                </script>
+                All Rights Reserved Term of use Union.
+                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+              </p>
+            </div>
+          </div>
+          <div class="col-lg-4">
+            <div class="footer_icon social_icon">
+              <ul class="list-unstyled">
+                <li>
+                  <a href="#" class="single_social_icon"><i class="fab fa-facebook-f"></i></a>
+                </li>
+                <li>
+                  <a href="#" class="single_social_icon"><i class="fab fa-twitter"></i></a>
+                </li>
+                <li>
+                  <a href="#" class="single_social_icon"><i class="fas fa-globe"></i></a>
+                </li>
+                <li>
+                  <a href="#" class="single_social_icon"><i class="fab fa-behance"></i></a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <!--::footer_part end::-->
+
+
+  <!-- jquery plugins here-->
+  <script src="<?= base_url('js/jquery-1.12.1.min.js') ?>"></script>
+  <!-- popper js -->
+  <script src="<?= base_url('js/popper.min.js') ?>"></script>
+  <!-- bootstrap js -->
+  <script src="<?= base_url('js/bootstrap.min.js') ?>"></script>
+  <!-- easing js -->
+  <script src="<?= base_url('js/jquery.magnific-popup.js') ?>"></script>
+  <!-- swiper js -->
+  <script src="<?= base_url('js/swiper.min.js') ?>"></script>
+  <!-- swiper js -->
+  <script src="<?= base_url('js/masonry.pkgd.js') ?>"></script>
+  <!-- particles js -->
+  <script src="<?= base_url('js/owl.carousel.min.js') ?>"></script>
+  <script src="<?= base_url('js/jquery.nice-select.min.js') ?>"></script>
+  <!-- slick js -->
+  <script src="<?= base_url('js/slick.min.js') ?>"></script>
+  <script src="<?= base_url('js/jquery.counterup.min.js') ?>"></script>
+  <script src="<?= base_url('js/waypoints.min.js') ?>"></script>
+  <script src="<?= base_url('js/contact.js') ?>"></script>
+  <script src="<?= base_url('js/jquery.ajaxchimp.min.js') ?>"></script>
+  <script src="<?= base_url('js/jquery.form.js') ?>"></script>
+  <script src="<?= base_url('js/jquery.validate.min.js') ?>"></script>
+  <script src="<?= base_url('js/mail-script.js') ?>"></script>
+  <!-- custom js -->
+  <script src="<?= base_url('js/custom.js') ?>"></script>
+</body>
+
+</html>
